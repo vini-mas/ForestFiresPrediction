f671540a975cce3d45516223eaa1c4342ae4353f
Ana <acpagnoncelli@inf.ufrgs.br>
2021-10-24 16:33:07 -0300

    :sparkles: Add str function to inmet class

--- inmet.py
+++ inmet.py
@@ -3,3 +3,6 @@ class Inmet():
         self.day = day
         self.hour = hour
         self.city = city
+
+    def __str__(self):
+        return ("Day: "+ self.day + " \nHour: " + self.hour + " \nCity: " + self.city)

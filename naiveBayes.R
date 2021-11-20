cd350b601bb7120fb72550320e26a3253ff0a241
Augusto Zanella Bardini <azbardini@gmail.com>
2021-11-20 18:26:16 -0300

    Update naiveBayes script

--- naiveBayes.R
+++ naiveBayes.R
@@ -1,27 +1,74 @@
 library("arules")
-
-df <- read.csv("centralized_AMAZONAS-APUI-Amazonia.csv", row.names=NULL, sep =",")
-df[is.na(df)] <- 0
-breaks = seq(from = 0, to = 260, by = 10)
-print(breaks)
-df$fire_power_category <- discretize(df$fire_power, method = "fixed", breaks=breaks)
-
 library(caret)
-
-set.seed(7267166)
-
-#p is the percentage of data that goes to training
-trainIndex=createDataPartition(df$fire_power_category, p=0.7)$Resample1
-train=df[trainIndex, ]
-test=df[-traindIndex, ]
-
-#check balance
-print(table(df$fire_power_category))
-
-print(table(train$fire_power_category))
-
 library(e1071)
 
+discretize_into_gaps <- function(column, interval_gap)
+{
+  category_breaks = seq(from = min(column), to = max(column)+interval_gap, by = interval_gap)
+  return(discretize(column, method = "fixed", breaks=category_breaks))
+}
+
+# Read into a dataframe
+df <- read.csv("centralized_data/2020/centralized_AMAZONAS-APUI-Amazonia.csv", row.names=NULL, sep =",")
+
+# Remove NAs
+df[is.na(df)] <- 0
+
+# Create firepower categories
+df$fire_power_category <- discretize_into_gaps(df$fire_power, 30)
+
+# Visualizando a distribuicao dos fogos por dia
+barplot(df$fire_power,
+        names.arg=df$date,
+        main="Histograma - Distribuicao dos fogos por dia", 
+        xlab="Dia", 
+        ylab="Poder de Fogo")
+
+# Visualizando a humidade por dia
+plot(df$temp_median, 
+     main="Histograma - Distribuicao de humidade por dia", 
+     xlab="Dia", 
+     ylab="Poder de Fogo",
+)
+
+#Set a random seed for partitioning
+set.seed(Sys.time())
+
+#Create 2 partitions, 70% being the training, 30% being the testing
+trainingPercentage = 0.7
+trainIndex=createDataPartition(df$fire_power_category, p=trainingPercentage)$Resample1
+trainingPartition=df[trainIndex, ]
+testingPartition=df[-trainIndex, ]
+
+#Watch each partitions amount of data
+print(table(df$fire_power_category))
+print(table(trainingPartition$fire_power_category))
+print(table(testingPartition$fire_power_category))
+
+#Generate a classifier
 NBclassifier = naiveBayes(fire_power_category~hum_median+precip_sum, data=train)
 
+#Watch the classifier percentages
 print(NBclassifier)
+
+# Train with the training partition
+trainPred=predict(NBclassifier, newdata=trainingPartition, type = "class")
+trainTable=table(trainingPartition$fire_power_category, trainPred)
+
+# Train with the testing partition
+testPred=predict(NBclassifier, newdata=testingPartition, type="class")
+testTable=table(testingPartition$fire_power_category, testPred)
+
+# Generate the accuracy of each table
+trainAcc=(trainTable[1,1]+trainTable[2,2]+trainTable[3,3])/sum(trainTable)
+testAcc=(testTable[1,1]+testTable[2,2]+testTable[3,3])/sum(testTable)
+
+message("Contingency Table for Training Data")
+print(trainTable)
+
+message("Contingency Table for Test Data")
+print(testTable)
+
+message("Accuracy")
+print(round(cbind(trainAccuracy=trainAcc, testAccuracy=testAcc),3))
+
